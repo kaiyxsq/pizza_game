6869172e3a4e708d3e290965e31ab99859389f2c
ivnet1 <i.veres2004@gmail.com>
2025-12-17 23:37:20 +0100

    pomeee dva

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,55 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class TopDownMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Animator anim;
 
-    Vector2 movement;
+    // Sem si uložíme, akú veľkú máš postavu (napr. 0.2 alebo 1)
+    private float originalScaleX; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        // Zapamätáme si, akú veľkosť si nastavil v Inspectore (aby sme ju nepokazili)
+        originalScaleX = transform.localScale.x;
+    }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-    movement = movement.normalized;
-    }
+        // 1. Získame vstup pre OBA smery
+        float moveX = Input.GetAxisRaw("Horizontal"); // A/D alebo Šípky do strán
+        float moveY = Input.GetAxisRaw("Vertical");   // W/S alebo Šípky hore/dole
 
-    void FixedUpdate()
-    {
-         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // 2. Vytvoríme vektor pohybu
+        // .normalized je dôležité, aby si nešiel šikmo rýchlejšie ako rovno
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+
+        // 3. Aplikujeme pohyb
+        rb.velocity = movement * speed;
+
+        // 4. LOGIKA ANIMÁCIE
+        // Ak sa hýbeme (vektor nie je nula), spustíme beh
+        bool isMoving = movement.magnitude > 0;
+        anim.SetBool("isRunning", isMoving);
+
+        // 5. OTÁČANIE (Zrkadlenie)
+        // Otáčame len podľa X (doľava/doprava).
+        // Ak ideme len hore/dole, necháme postavu otočenú tak, ako bola naposledy.
+        if (moveX > 0)
+        {
+            // Doprava: Použijeme kladnú veľkosť
+            transform.localScale = new Vector3(Mathf.Abs(originalScaleX), transform.localScale.y, transform.localScale.z);
+        }
+        else if (moveX < 0)
+        {
+            // Doľava: Použijeme zápornú veľkosť
+            transform.localScale = new Vector3(-Mathf.Abs(originalScaleX), transform.localScale.y, transform.localScale.z);
+        }
     }
 }
